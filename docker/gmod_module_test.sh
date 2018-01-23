8bc439100b8291d23cf0e27a427ba169234282bd
Brandon Boudreaux <boudreaux00@icloud.com>
2018-01-23 04:26:30 -0500

    Build setup complete

--- docker/gmod_module_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -it \
-	-p 27016:27016/udp \
-	-e "port=27016" \
-	-v ${PWD}/module:/opt/garrysmod/garrysmod/lua/bin:rw \
-	hackebein/garrysmod /bin/bash #LD_LIBRARY_PATH=garrysmod/bin:bin:. LD_DEBUG=libs ./srcds_run -game garrysmod -port 27016 +gamemode sandbox +map gm_flatgrass
-
